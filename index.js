c235bc41796bbca5a995bbb329513daeb8b092e9
Gabriele D'Incà <gabriele.dinca05@gmail.com>
2026-02-04 15:56:58 +0100

    Eliminazione del file di connessione al db

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import app from './app/app.js';
-import { connectDB } from './app/utils/db.js';
+import mongoose from 'mongoose';
 const PORT = process.env.PORT || 8080;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log('Database connesso');
+  } catch (err) {
+    console.error('Errore di connessione al Database:', err);
+    process.exit(1);
+  }
+}
 
 // Funzione che tenta la connessione a DB Mongo Atlas
 await connectDB();
