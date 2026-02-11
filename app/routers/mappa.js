1717fa2f8296e58cca595d32880027dbb092c971
Gabriele D'Incà <gabriele.dinca05@gmail.com>
2026-02-11 14:54:38 +0100

    Aggiunta API GET /mappa che ritorna segnalazioni e aree

--- app/routers/mappa.js
+++ app/routers/mappa.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import Segnalazione from '../models/segnalazione.js';
+import Area from '../models/area.js';
 
 const router = express.Router();
 
 /*----------------------------------------
- - GET: TODO (!!!)
+ - GET: Get delle aree e delle segnalazioni
 ------------------------------------------*/
 router.get('/', async (req,res) => {
     try {
         // Cerco sul DB tutte le segnalazioni marcate come 'Validate'
         const segnalazioni = await Segnalazione.find({ stato: 'Validata' }).exec();
-        /*res.status(200).json({
+
+        const aree = await Area.find({}).exec();
+
+        return res.status(200).json({
             success: true,
-            segnalazioni
-        )};*/
+            segnalazioni,
+            aree
+        });
     } catch (err) {
         return res.status(500).json({
             success: false,
